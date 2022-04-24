26bb9cfe931d82fb83eea96220ced8792972ec5f
Thiago Kramer <kramergonzalez@gmail.com>
2022-04-24 01:45:36 -0300

    comit de hoje

--- Pneumonia Detection/pneumonia.py
+++ Pneumonia Detection/pneumonia.py
@@ -1,2 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import auc
+import keras
+import tensorflow as tf
+from keras import backend as K
+from keras import metrics
+from keras.regularizers import 12
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization, InputLayer, Activation
+from keras.metrics import AUC
+from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping
+from sklearn.metrics import classification_report, confusion_matrix
+
+import os
+import random
+
+# Set a seed value
+seed_value = 42
+
+# Setting python env variable at a fixed value
+os.environ['PYTHONHASHSEED'] = str(seed_value)
+random.seed(seed_value)
+np.random.seed(seed_value)
+tf.set_random_seed(seed_value)
+
+session_conf = tf.ConfigProto(intra_op_parallelism_threads = 1, inter_op_parallelism_threads = 1)
+sess = tf.Session(graph = tf.get_default_graph(), config = session_conf)
+K.set_session(sess)
